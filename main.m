70cf326d7ef726a5bb2c5130d82a34069e7fe64b
Adi Pasic <ajpasic@users.noreply.github.com>
2018-07-29 13:00:29 -0500

    cleaned up code

--- main.m
+++ main.m
@@ -15,58 +15,26 @@ pins = Adi_pins;
             
 % create a board "mask"
 board = zeros(3);
-board(4) = 1;
+board(1) = 1;
 
 % write values to pins
 UpdateBoard(a,board, pins)
 
-z = 0;
-while z ~= 13 % while enter key has not been pressed
-    z = GetKey;
-    [row, col] = find(board == 1); % find position of current LED
-    switch z
-        case 28
-            % we pressed the left key
-            if col - 1 < 1 
-                disp('that is not a valid movement')
-                continue
-            else 
-                board(row, col - 1) = 1;
-            end
-        case 29
-            % we pressed the right key
-            if col + 1 > 3 
-                disp('that is not a valid movement')
-                continue
-            else 
-                board(row, col + 1) = 1;
-            end
-        case 30
-            % we pressed the up key
-            if row - 1 < 1 
-                disp('that is not a valid movement')
-                continue
-            else 
-                board(row - 1, col) = 1;
-            end
-        case 31
-            % we pressed the down key
-            if row + 1 > 3 
-                disp('that is not a valid movement')
-                continue
-            else 
-                board(row + 1, col) = 1;
-            end
-        case 13
-            % we pressed the enter key - the loop will exit
-        otherwise
-            % a bad key was pressed, prompt user for another key press
-            disp('INCORRECT')
-    end
-    if z ~= 13 % prevents board from being erased on LED selection
-        board(row, col) = 0; % erase old position
-    end
-    UpdateBoard(a, board, pins) % update the board
-end
+% make ship locations array
+NumShips = 1;
+shipLoc = ShipLocations(NumShips);
 
-    
+[row, col] = MakeSelection(a, board, pins);
+
+% compare selection to ship location(s)
+for i = 1:1:8
+    if shipLoc(row, col) == 1 % if we hit the ship
+        disp('Hit')
+        break
+    else
+        disp(['Miss,', num2str(8-i), ' tries remaining'])
+        [row, col] = MakeSelection(a, board, pins);
+    end
+    disp(shipLoc)
+    disp([row, col])
+end    
