896ac4c41ce6f4f6cde47e4592f68d912683e5f7
Vincent Laroche <vvslaroche@gmail.com>
2018-10-23 13:46:00 -0500

    Deleted redundant files

--- UpdateBoard.m
+++
@@ -1,8 +0,0 @@
-function UpdateBoard(ard, pins, board)
-
-for c = 1:9
-    writeDigitalPin(ard, pins(c), board(c));
-%     writeDigitalPin(ard, Vincent_pins(c), board_matrix(c));
-end
-end
-
--- pauseTone.m
+++
@@ -1,4 +0,0 @@
-function pauseTone(arduino, pin, pitch, duration)
-playTone(arduino, pin, pitch, duration)
-pause(duration)
-end
